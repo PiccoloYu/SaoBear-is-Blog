b5e4debebc794ec4332c93da71e984484d6252f1
piccoloyu <1490169007@qq.com>
2019-10-19 17:09:07 +0800

    测试

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -2,22 +2,23 @@ import React, { Component } from 'react';
 import 'gitalk/dist/gitalk.css'
 import Gitalk from 'gitalk'
 
-const gitalk = new Gitalk({
-  clientID: 'd06130056fa27590d47e',
-  clientSecret: '848534155ec3e9f5775ca97f537b3ecfa6dc5dcd',
-  repo: 'Blog-Comment',
-  owner: 'PiccoloYu',
-  admin: ['PiccoloYu'],
-  id: window.location.pathname,      // Ensure uniqueness and length less than 50
-  distractionFreeMode: true, // Facebook-like distraction free mode
-  createIssueManually: false
-})
+
 
 
 
 class Comment extends Component {
 
   componentDidMount() {
+    const gitalk = new Gitalk({
+      clientID: 'd06130056fa27590d47e',
+      clientSecret: '848534155ec3e9f5775ca97f537b3ecfa6dc5dcd',
+      repo: 'Blog-Comment',
+      owner: 'PiccoloYu',
+      admin: ['PiccoloYu'],
+      id: window.location.pathname,      // Ensure uniqueness and length less than 50
+      distractionFreeMode: true, // Facebook-like distraction free mode
+      createIssueManually: false
+    })
     gitalk.render('gitalk-container')
   }
 
